1153037d9865d8a6bbfce26ea66274a2f2a9c010
Mohanraj1100 <123471382+Mohanraj1100@users.noreply.github.com>
2023-02-12 22:16:09 +0530

    Update Frequency.java

--- src/main/java/assignment/frequency/Frequency.java
+++ src/main/java/assignment/frequency/Frequency.java
@@ -7,20 +7,23 @@ import java.util.PriorityQueue;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.*;
 public class Frequency 
 {
 
 	public static void main( String[] args ) throws FileNotFoundException
     {
-        File f = new File("C:\\Users\\Tringapps-User25\\freq.txt");
+	String address = "C:\\Users\\Tringapps-User25\\freq.txt";
+        File f = new File(address);
         String str = "";
         Scanner sc = new Scanner(f);
         Logger logger = Logger.getLogger("hi");
+	StringBuilder sb = new StringBuilder();
         HashMap<String,Integer> wordcount = new HashMap<>();
         while (sc.hasNextLine())
         {
-        	str += sc.nextLine();
-        	str+= " ";
+        	str.append(sc.nextLine());
+        	str.append(" ");
         }
         String[] words = str.split(" ");
         for(int i=0;i<words.length;i++)
@@ -38,10 +41,7 @@ public class Frequency
         PriorityQueue<HashMap.Entry<String,Integer>> pq = new PriorityQueue<>((a,b)-> {
         	return b.getValue() - a.getValue();
         });    
-        	for(HashMap.Entry<String,Integer> e : wordcount.entrySet())
-        	{
-        		pq.add(e);
-        	}
+        	pq.addAll(wordcount.entrySet());
         	while(!pq.isEmpty())
         	{
         		logger.log(Level.INFO,()->"\n"+pq.poll());
